6cd9126b08d0601e1a99748ca27697b4111f4c4a
Santiago Gallego <santiagoxgallego@gmail.com>
2025-11-17 19:23:36 -0500

    mejorado clases

---
+++ Operadores-Funciones/script.js
@@ -0,0 +1,84 @@
+        var a=2;
+        var b=5;
+        var potencia25=a**b;
+        var modulo52=a%b;
+
+        /*Operadores Aritmeticos*/
+        document.getElementById("potencia25").innerHTML=potencia25;
+        document.getElementById("modulo52").innerHTML=modulo52;
+        document.getElementById("incremento").innerHTML=a++;
+        document.getElementById("decremento").innerHTML=a--;
+        document.getElementById("suma").innerHTML=a+=1;
+        document.getElementById("resta").innerHTML=a-=1;
+        document.getElementById("multiplicacion").innerHTML=a*=1;
+        document.getElementById("division").innerHTML=a/=1;
+
+        /*Operadores Logicos*/
+        document.getElementById("and").innerHTML=a&&b;
+        document.getElementById("or").innerHTML=a||b;
+        document.getElementById("not").innerHTML=!a;
+
+        /*Operadores de Comparacion*/
+        document.getElementById("igualdad").innerHTML=a==b;
+        document.getElementById("desigualdad").innerHTML=a!=b;
+        document.getElementById("igualdadEstricta").innerHTML=a===b;
+        document.getElementById("desigualdadEstricta").innerHTML=a!==b;
+        document.getElementById("mayorQue").innerHTML=a>b;
+        document.getElementById("menorQue").innerHTML=a<b;
+        document.getElementById("mayorOIgualQue").innerHTML=a>=b;
+        document.getElementById("menorOIgualQue").innerHTML=a<=b;
+        document.getElementById("igualdadEstricta").innerHTML=a===b;
+        document.getElementById("desigualdadEstricta").innerHTML=a!==b;
+
+        /*Funciones
+        function funcionSinParametros(){
+            alert("Hola mundo");
+        }
+        funcionSinParametros();
+
+        function funcionConParametros(parametro1, parametro2){
+            alert(parametro1+parametro2);
+        }
+        funcionConParametros("Hola ", "mundo");
+
+        function funcionConRetorno(parametro1, parametro2){
+            return parametro1+parametro2;
+        }
+        var resultado=funcionConRetorno("Hola ", "mundo");
+        alert(resultado);   
+
+        function funcionFlecha(parametro1, parametro2){
+            return parametro1+parametro2;
+        }
+
+        var resultado=funcionFlecha("Hola ", "mundo");
+        alert(resultado);   
+
+        function funcionConstructora(parametro1, parametro2){
+            return parametro1+parametro2;
+        }
+
+        var resultado=funcionConstructora("Hola ", "mundo");
+        alert(resultado);   
+
+        function funcionAnonima(parametro1, parametro2){
+            return parametro1+parametro2;
+        }
+
+        var resultado=funcionAnonima("Hola ", "mundo");
+        alert(resultado);   
+
+        function funcionCallback(parametro1, parametro2){
+            return parametro1+parametro2;
+        }
+
+        var resultado=funcionCallback("Hola ", "mundo");
+        alert(resultado);   
+
+        function funcionRecursiva(parametro1, parametro2){
+            return parametro1+parametro2;
+        }
+
+        var resultado=funcionRecursiva("Hola ", "mundo");
+        alert(resultado);   
+*/
